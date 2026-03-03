2876612af1f93f0d3efe24d907b74826f14c3bb7
Dexterity Wurayayi <dexterity.wurayayi@qatctech.com>
2026-03-03 16:25:58 +0200

    add more ads

--- core/resources/views/eden/home.php
+++ core/resources/views/eden/home.php
@@ -89,6 +89,11 @@
   </div>
 </section>
 
+<div style="display:flex; justify-content:center; padding:16px 0;">
+  <div class="MainAdverTiseMentDiv" data-publisher="eyJpdiI6InpsbjBkRVNsSTg0YVpndEFVdCt1Mmc9PSIsInZhbHVlIjoiUnJTUHc3TzRpT3UzVWxZR3ozL0xidz09IiwibWFjIjoiMTk2MTE2YTk1YmUxZmRlZGFlMzRhNmQ2ZGRmY2E5MDBhZWQwYjk4Mjc2MDhiNmZjNmJlYTM2MjAyZDdiMDRjYiIsInRhZyI6IiJ9" data-adsize="970x90"></div>
+  <script class="adScriptClass" src="https://zimadsense.com/assets/ads/ad.js"></script>
+</div>
+
 <div class="wrap">
   <?php $sortNewest = $sortNewest ?? false; ?>
   <?php $launchingToday = $launchingToday ?? collect(); ?>
@@ -239,6 +244,11 @@
     </script>
   </section>
   <?php endif; ?>
+
+  <div style="display:flex; justify-content:center; padding:16px 0;">
+    <div class="MainAdverTiseMentDiv" data-publisher="eyJpdiI6InpsbjBkRVNsSTg0YVpndEFVdCt1Mmc9PSIsInZhbHVlIjoiUnJTUHc3TzRpT3UzVWxZR3ozL0xidz09IiwibWFjIjoiMTk2MTE2YTk1YmUxZmRlZGFlMzRhNmQ2ZGRmY2E5MDBhZWQwYjk4Mjc2MDhiNmZjNmJlYTM2MjAyZDdiMDRjYiIsInRhZyI6IiJ9" data-adsize="970x90"></div>
+    <script class="adScriptClass" src="https://zimadsense.com/assets/ads/ad.js"></script>
+  </div>
 
   <?php $justListed = $justListed ?? collect(); ?>
   <?php if (!$sortNewest && $justListed->isNotEmpty()): ?>
